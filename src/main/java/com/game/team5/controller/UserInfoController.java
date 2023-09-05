da738a8bec5fef81176d66206bf1adc5628bd227
songkungsug <songkungsug@gmail.com>
2023-09-05 13:25:09 +0900

    commit

--- src/main/java/com/game/team5/controller/UserInfoController.java
+++ src/main/java/com/game/team5/controller/UserInfoController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,9 +32,20 @@ public class UserInfoController {
 		return uiService.selectUserInfo(uiNum);
 	}
 	
-	@PutMapping("/user-infos")
+	@PostMapping("/user-infos")
 	public int insertUserInfo(@RequestBody UserInfoVO user) {
 		return uiService.insertUserInfo(user);
 	}
+	
+	@PutMapping("/user-infos")
+	public int updateUserInfo(UserInfoVO user) {
+		return uiService.updateUserInfo(user);
+	}
+	
+	@DeleteMapping("/user-infos/{uiNum}")
+	public int deleteUserInfo(@PathVariable int uiNum) {
+		return uiService.deleteUserinfo(uiNum);
+	}
+	
 	
 }
